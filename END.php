c86965d657325616cb6b7a2bc53757c2940a69a5
Quang_Trần <54528823+txquang@users.noreply.github.com>
2020-10-08 19:08:37 +0700

    Create END.php

---
+++ END.php
@@ -0,0 +1,17 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="CSS/END.css">
+</head>
+<body>
+	<div id="cuoi">
+		<p><img class="anh" src="END\FACEBOOK.png">Truyen Tranh</p>
+		<p><img class="anh" src="END\GMAIL.png">truyentranh@gmail.com</p>
+		<p><img class="anh" src="END\DIEN_THOAI.png">0123456789</p><br><br>
+	    <p id="tr">© 2018 Copyright: Trần Xuân Quang, Vũ Anh Tài</p>
+	</div>
+</body>
+</html>
