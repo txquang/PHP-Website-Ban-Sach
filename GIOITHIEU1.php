cf2ba1822504adb2bd9b41a9936a3be865ac5b56
Quang_Trần <54528823+txquang@users.noreply.github.com>
2020-10-08 19:08:49 +0700

    Create GIOITHIEU1.php

---
+++ GIOITHIEU1.php
@@ -0,0 +1,29 @@
+<?php
+	include 'TOP.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Gioi Thieu</title>
+	<link rel="stylesheet" type="text/css" href="CSS/GIOITHIEU.css">
+</head>
+<body>
+	<div align="center"  id="tieude">
+		<h1>Đề tài: Website bán sách PHP</h1>
+		<h2>Người thực hiện</h2>
+	</div>
+	<div>
+		<h2> Trần Xuân Quang - 18I3  - txquang.18i@cit.udn.vn</h2>
+		<a href="https://www.facebook.com/Quang16072000"><img src="ANHGIOITHIEU/2.png"></a>
+	</div>
+	<div>
+		<h2> Vũ Anh Tài - 18I3- vatai.18i@cit.udn.vn</h2>
+
+		<a href="https://www.facebook.com/VuAnhTai01"><img src="ANHGIOITHIEU/1.png"></a>
+	</div>
+	
+</body>
+</html>
+<?php
+  include 'END.php';
+?>
